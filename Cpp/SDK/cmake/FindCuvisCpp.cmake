ef9a9638dc1b57544f975f98903ebf74c7cd687c
Arnd <brandes@cubert-gmbh.de>
2023-02-02 13:19:03 +0100

    * changed c imported target name from cuvis::cuvis to cuvis::c * added cuvis::cpp shared library target, while using the included cuvis.hpp included wrapper from this repo.

--- Cpp/SDK/cmake/FindCuvisCpp.cmake
+++ Cpp/SDK/cmake/FindCuvisCpp.cmake
@@ -11,18 +11,22 @@ find_path(CUVIS_INCLUDE_DIR
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(Cuvis DEFAULT_MSG
+find_package_handle_standard_args(CuvisCpp DEFAULT_MSG
                                   CUVIS_LIBRARY
                                   CUVIS_INCLUDE_DIR)
 
 mark_as_advanced(CUVIS_LIBRARY CUVIS_INCLUDE_DIR)
 
-if(Cuvis_FOUND AND NOT TARGET cuvis::c)
-  add_library(cuvis::c STATIC IMPORTED)
+if(CuvisCpp_FOUND AND NOT TARGET cuvis::cpp)
+	
+  add_library(cuvis::cpp STATIC IMPORTED)
+  
+  #simmilar to the c library, we use the cuvis.dll, howver we add 
+  #the cpp interface file as well as force the utilizing target to switch to c++17
   set_target_properties(
-    cuvis::c
+    cuvis::cpp
     PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${CUVIS_INCLUDE_DIR}"
+      INTERFACE_INCLUDE_DIRECTORIES "${CUVIS_INCLUDE_DIR};${CMAKE_CURRENT_LIST_DIR}/../interface"
       IMPORTED_LOCATION ${CUVIS_LIBRARY})
-
+    target_compile_features(cuvis::cpp INTERFACE cxx_std_17)
 endif()
