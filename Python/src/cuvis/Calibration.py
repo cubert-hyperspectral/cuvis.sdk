2464a78de79838b779d0642b46687e9b976e39f6
Benjamin Müller <mueller@cubert-gmbh.de>
2023-05-10 15:35:35 +0200

    fix formatting and removing unused imports

--- Python/src/cuvis/Calibration.py
+++ Python/src/cuvis/Calibration.py
@@ -1,13 +1,14 @@
 from . import cuvis_il
-from .cuvis_aux import SDKException, __bit_translate__
-from .cuvis_types import ProcessingMode, OperationMode
+from .cuvis_aux import SDKException
+from .cuvis_types import OperationMode
 
 
 class Calibration(object):
     def __init__(self, calibdir=None):
         self.__handle__ = None
         _ptr = cuvis_il.new_p_int()
-        if cuvis_il.status_ok != cuvis_il.cuvis_calib_create_from_path(calibdir, _ptr):
+        if cuvis_il.status_ok != cuvis_il.cuvis_calib_create_from_path(calibdir,
+                                                                       _ptr):
             raise SDKException()
         self.__handle__ = cuvis_il.p_int_value(_ptr)
         pass
@@ -20,7 +21,8 @@ class Calibration(object):
         _ptr = cuvis_il.new_p_int()
         for k, v in All_Modes.items():
             try:
-                if cuvis_il.status_ok != cuvis_il.cuvis_calib_get_capabilities(self.__handle__, v, _ptr):
+                if cuvis_il.status_ok != cuvis_il.cuvis_calib_get_capabilities(
+                        self.__handle__, v, _ptr):
                     raise SDKException()
                 else:
                     Usable_Modes.append(k)
--- Python/src/cuvis/Viewer.py
+++ Python/src/cuvis/Viewer.py
@@ -1,5 +1,3 @@
-import cuvis.cuvis_il
-
 from . import cuvis_il
 from .Measurement import ImageData
 from .cuvis_aux import SDKException
@@ -15,45 +13,57 @@ class Viewer(object):
             self.__handle__ = settings
         if isinstance(self.ViewerSettings, cuvis_il.cuvis_viewer_settings_t):
             _ptr = cuvis_il.new_p_int()
-            if cuvis_il.status_ok != cuvis_il.cuvis_viewer_create(_ptr, self.ViewerSettings):
+            if cuvis_il.status_ok != cuvis_il.cuvis_viewer_create(_ptr,
+                                                                  self.ViewerSettings):
                 raise SDKException()
             self.__handle__ = cuvis_il.p_int_value(_ptr)
         else:
-            raise SDKException("Could not open viewer_settings of type {}!".format(type(self.ViewerSettings)))
+            raise SDKException(
+                "Could not open viewer_settings of type {}!".format(
+                    type(self.ViewerSettings)))
 
         pass
 
     def getDataCount(self, mesu):
         _ptr = cuvis_il.new_p_int()
-        if cuvis_il.status_ok != cuvis_il.cuvis_viewer_apply(self.__handle__, mesu.__handle__, _ptr):
+        if cuvis_il.status_ok != cuvis_il.cuvis_viewer_apply(self.__handle__,
+                                                             mesu.__handle__,
+                                                             _ptr):
             raise SDKException()
         currentView = cuvis_il.p_int_value(_ptr)
 
         countHandle = cuvis_il.new_p_int()
-        if cuvis_il.status_ok != cuvis_il.cuvis_view_get_data_count(currentView, countHandle):
+        if cuvis_il.status_ok != cuvis_il.cuvis_view_get_data_count(currentView,
+                                                                    countHandle):
             raise SDKException()
 
         return cuvis_il.p_int_value(countHandle)
 
     def apply(self, mesu):
         _ptr = cuvis_il.new_p_int()
-        if cuvis_il.status_ok != cuvis_il.cuvis_viewer_apply(self.__handle__, mesu.__handle__, _ptr):
+        if cuvis_il.status_ok != cuvis_il.cuvis_viewer_apply(self.__handle__,
+                                                             mesu.__handle__,
+                                                             _ptr):
             raise SDKException()
         currentView = cuvis_il.p_int_value(_ptr)
 
         countHandle = cuvis_il.new_p_int()
-        if cuvis_il.status_ok != cuvis_il.cuvis_view_get_data_count(currentView, countHandle):
+        if cuvis_il.status_ok != cuvis_il.cuvis_view_get_data_count(currentView,
+                                                                    countHandle):
             raise SDKException()
 
         dataCount = cuvis_il.p_int_value(countHandle)
 
         for i in range(dataCount):
             view_data = cuvis_il.cuvis_view_data_t()
-            if cuvis_il.status_ok != cuvis_il.cuvis_view_get_data(currentView, i, view_data):
+            if cuvis_il.status_ok != cuvis_il.cuvis_view_get_data(currentView,
+                                                                  i, view_data):
                 raise SDKException()
 
-            if view_data.data.format == CUVIS_imbuffer_format["imbuffer_format_uint8"]:
-                return ImageData(img_buf=view_data.data, dformat=view_data.data.format)
+            if view_data.data.format == CUVIS_imbuffer_format[
+                "imbuffer_format_uint8"]:
+                return ImageData(img_buf=view_data.data,
+                                 dformat=view_data.data.format)
             else:
                 raise SDKException("Unsupported viewer bit depth!")
 
