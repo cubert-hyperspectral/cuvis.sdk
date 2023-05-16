4212878fd837e139cdf0495cb4ed11bb79979617
Benjamin Müller <mueller@cubert-gmbh.de>
2023-05-16 15:11:55 +0200

    update to Worker.py (ID)

--- Python/src/cuvis/Worker.py
+++ Python/src/cuvis/Worker.py
@@ -10,6 +10,7 @@ class Worker(object):
         self.acquisitionSet = False
         self.processingSet = False
         self.viewerSet = False
+        self.sessionFileSet = False
 
         self.__handle__ = None
         _ptr = cuvis_il.new_p_int()
@@ -21,54 +22,92 @@ class Worker(object):
 
     def setAcquisitionContext(self, base=None):
         if base is not None:
-            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_acq_cont(self.__handle__, base.__handle__):
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_acq_cont(
+                    self.__handle__, base.__handle__):
                 raise SDKException()
         else:
-            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_acq_cont(self.__handle__, 0):
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_acq_cont(
+                    self.__handle__, 0):
                 raise SDKException()
         self.acquisitionSet = True
         pass
 
     def setProcessingContext(self, base=None):
         if base is not None:
-            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_proc_cont(self.__handle__, base.__handle__):
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_proc_cont(
+                    self.__handle__, base.__handle__):
                 raise SDKException()
         else:
-            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_proc_cont(self.__handle__, 0):
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_proc_cont(
+                    self.__handle__, 0):
                 raise SDKException()
         self.processingSet = True
         pass
 
     def setExporter(self, base=None):
         if base is not None:
-            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_exporter(self.__handle__, base.__handle__):
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_exporter(
+                    self.__handle__, base.__handle__):
                 raise SDKException()
         else:
-            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_exporter(self.__handle__, 0):
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_exporter(
+                    self.__handle__, 0):
                 raise SDKException()
         self.exporterSet = True
         pass
 
     def setViewer(self, base=None):
         if base is not None:
-            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_viewer(self.__handle__, base.__handle__):
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_viewer(
+                    self.__handle__, base.__handle__):
                 raise SDKException()
         else:
-            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_viewer(self.__handle__, 0):
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_viewer(
+                    self.__handle__, 0):
                 raise SDKException()
         self.viewerSet = True
         pass
 
+    def setSessionFile(self, base=None, skipDroppedFrames=True):
+        if base is not None:
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_session_file(
+                    self.__handle__, skipDroppedFrames, base.__handle__):
+                raise SDKException()
+        else:
+            if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_session_file(
+                    self.__handle__, skipDroppedFrames, 0):
+                raise SDKException()
+        self.viewerSet = True
+        pass
+
+    def ingestMesu(self, mesu):
+        if cuvis_il.status_ok != cuvis_il.cuvis_worker_ingest_mesu(
+                self.__handle__, mesu):
+            raise SDKException()
+        pass
+
+    def querySessionProgress(self):
+        frames_read = cuvis_il.new_p_int()
+        frames_total = cuvis_il.new_p_int()
+        if cuvis_il.status_ok != \
+                cuvis_il.cuvis_worker_query_session_progress(self.__handle__,
+                                                             frames_read,
+                                                             frames_total):
+            raise SDKException()
+        return dict({"frames_read": frames_read, "frames_total": frames_total})
+
     def hasNextResult(self):
         val = cuvis_il.new_p_int()
-        if cuvis_il.status_ok != cuvis_il.cuvis_worker_has_next_result(self.__handle__, val):
+        if cuvis_il.status_ok != cuvis_il.cuvis_worker_has_next_result(
+                self.__handle__, val):
             raise SDKException()
         return cuvis_il.p_int_value(val) != 0
 
     def getNextResult(self):
         this_mesu = cuvis_il.new_p_int()
         this_viewer = cuvis_il.new_p_int()
-        if cuvis_il.status_ok != cuvis_il.cuvis_worker_get_next_result(self.__handle__, this_mesu, this_viewer):
+        if cuvis_il.status_ok != cuvis_il.cuvis_worker_get_next_result(
+                self.__handle__, this_mesu, this_viewer):
             raise SDKException()
         mesu = Measurement(cuvis_il.p_int_value(this_mesu))
         if self.viewerSet:
@@ -80,18 +119,21 @@ class Worker(object):
 
     def getQueueSize(self):
         val = cuvis_il.new_p_int()
-        if cuvis_il.status_ok != cuvis_il.cuvis_worker_get_queue_limit(self.__handle__, val):
+        if cuvis_il.status_ok != cuvis_il.cuvis_worker_get_queue_limit(
+                self.__handle__, val):
             raise SDKException()
         return cuvis_il.p_int_value(val)
 
     def setQueueSize(self, val):
-        if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_queue_limit(self.__handle__, val):
+        if cuvis_il.status_ok != cuvis_il.cuvis_worker_set_queue_limit(
+                self.__handle__, val):
             raise SDKException()
         pass
 
     def getQueueUsed(self):
         val = cuvis_il.new_p_int()
-        if cuvis_il.status_ok != cuvis_il.cuvis_worker_get_queue_used(self.__handle__, val):
+        if cuvis_il.status_ok != cuvis_il.cuvis_worker_get_queue_used(
+                self.__handle__, val):
             raise SDKException()
         return cuvis_il.p_int_value(val)
 
