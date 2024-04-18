412c2471a35bae73095fcea33c0f65924b75ea89
Simon Birkholz <birkholz@cubert-gmbh.de>
2024-04-18 11:35:51 +0200

    wip

--- regenerate_readme.sh
+++ regenerate_readme.sh
@@ -4,7 +4,8 @@
 cd $(git rev-parse --show-toplevel)
 
 cat readme/logo.md > README.md
-echo "# cuvis.sdk" >> README.md
+echo "# cuvis.sdk\n" >> README.md
 cat readme/header.md >> README.md
+echo "\n" >> README.md
 cat README.in >> README.md
 cat readme/footer.md >> README.md
